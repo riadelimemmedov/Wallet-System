9e441c3c0bb8c1eed7aee13e23c4900b7b5ba7cc
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2025-01-26 17:25:42 +0400

    Divide application file structure do clean and reuse format

---
+++ util/env/environment.go
@@ -0,0 +1,23 @@
+package env
+
+import (
+	"fmt"
+
+	"github.com/riad/banksystemendtoend/util/config"
+)
+
+func NewAppEnvironmentConfig(environment string) (config.AppConfig, error) {
+	appConfigMap := map[string]config.AppConfig{
+		"test": {
+			ConfigFilePath: "../../.env.test",
+		},
+		"dev": {
+			ConfigFilePath: "../../.env.dev",
+		},
+	}
+	appConfig, exists := appConfigMap[environment]
+	if !exists {
+		return config.AppConfig{}, fmt.Errorf("environment %s not found", environment)
+	}
+	return appConfig, nil
+}
